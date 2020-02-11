aaf3e99a76b4f8f5d9ee690e35f5953e6d25b0ee
Matt Munch <mattmunch94@gmail.com>
2020-02-11 14:42:56 -0800

    functioning properly

--- src/components/recipeForm/RecipeForm.js
+++ src/components/recipeForm/RecipeForm.js
@@ -5,7 +5,7 @@ const RecipeForm = () => {
   const [recipeName, setRecipeName] = useState('');
   const [directions, setDirections] = useState([]);
   const [ingredients, setIngredients] = useState([]);
-  const ingredientInputs = ingredients.map(({ name, amount, unit }, i) => {
+  const ingredientInputs = ingredients.map(({ name, amount, measurement }, i) => {
     const handleChange = (key) => ({ target }) => setIngredients(oldIngredients => oldIngredients.map((oi, oiIndex) => {
       if(oiIndex === i) return { ...oi, [key]: target.value };
       return oi;
@@ -13,8 +13,8 @@ const RecipeForm = () => {
     return (
       <div key={i} >
         <input type="text" value={name} onChange={handleChange('name')} placeholder="Ingredient Name"/>
-        <input type="number" value={amount} onChange={handleChange('amount')} placeholder="Amount" />
-        <select defaultValue={unit} onChange={handleChange('unit')}>
+        <input type="number" min='0' value={amount} onChange={handleChange('amount')} placeholder="Amount" />
+        <select defaultValue={measurement} onChange={handleChange('measurement')}>
           <option value="cup">Cup</option>
           <option value="tablespoon">Tablespoon</option>
           <option value="teaspoon">Teaspoon</option>
@@ -25,15 +25,37 @@ const RecipeForm = () => {
       </div>
     );
   });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    return fetch('https://fathomless-meadow-03057.herokuapp.com/api/v1/recipes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        origin: true
+      },
+      body: JSON.stringify({
+        name: recipeName,
+        ingredients: ingredients,
+        directions: directions 
+      })
+    })
+      .then(res => Promise.all([res.ok, res.json()]))
+      .then(([ok, json]) => {
+        if(!ok) throw json;
+        return json;
+      });
+    
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         <input type="text"  value={recipeName} onChange={({ target }) => setRecipeName(target.value)} placeholder="Recipe Name"/>
       </label>
       <label>
         <textarea type="text" value={directions} onChange={({ target }) => setDirections(target.value)} placeholder="Directions"/>
       </label>
-      <button type="button" onClick={() => setIngredients(oi => [...oi, { name: '', amount: 0, unit: 'grams' }])}>Add An Ingredient</button>
+      <button type="button" onClick={() => setIngredients(oi => [...oi, { name: '', amount: 0, measurement: 'grams' }])}>Add An Ingredient</button>
       {ingredientInputs}
       <input type="submit" value="Submit"/>
     </form>
