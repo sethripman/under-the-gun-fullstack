788d1a9c6311b886e425b70b49423f79f34ea29f
Eve Stockstill <evestockstillpdx@gmail.com>
2020-02-11 09:54:52 -0800

    some part of nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Nav from './nav/Nav';
+
 import RecipeList from './recipes/RecipeList';
-import RecipeDetail from './recipes/RecipeDetail';
+// import RecipeDetail from './recipes/RecipeDetail';
 // import { Router, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
-      <RecipeList />;
-      <RecipeDetail />
+      <Nav />
+      <RecipeList />
+      {/* <RecipeDetail /> */}
     </>
   );
 }
