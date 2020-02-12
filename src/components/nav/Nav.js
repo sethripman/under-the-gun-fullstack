a2a3d79aac3c45f07d38f1f820b28d5977d16332
Eve Stockstill <evestockstillpdx@gmail.com>
2020-02-11 17:55:45 -0800

    working on form styling

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -6,9 +6,10 @@ import styles from './Nav.css';
 const Nav = () => {
   return (
     <nav className={styles.nav}>
-      <img src={logo} alt='' />
-      <h1 className={styles.title}>Recipes to Die For</h1>
-
+      {/* <img src={logo} alt='' /> */}
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>Recipes to Die For</h1>
+      </div>
       <ul className={styles.nav_links}>
         <NavLink to='/' className={styles.nav_link}>
           <li>Recipes</li>
@@ -16,7 +17,6 @@ const Nav = () => {
         <NavLink to='/recipeForm' className={styles.nav_link}>
           <li>Add Recipe</li>
         </NavLink>
-        
       </ul>
     </nav>
   );
