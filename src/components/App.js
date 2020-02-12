67b1f3c32348035ce34bc1d61477105e779266c5
Eve Stockstill <evestockstillpdx@gmail.com>
2020-02-11 16:01:51 -0800

    nav done

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import Nav from './nav/Nav';
 import RecipeList from './recipes/RecipeList';
 import RecipeDetail from './recipes/RecipeDetail';
+import RecipeForm from './recipeForm/RecipeForm';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
       <Router>
-        <Route path='/' component={Nav} />
+        <Nav />
         <Route exact path='/' component={RecipeList} />
         <Route path='/recipeDetail/:recipe_id' component={RecipeDetail} />
+        <Route path= '/recipeForm' component={RecipeForm} />
       </Router>
     </>
   );
